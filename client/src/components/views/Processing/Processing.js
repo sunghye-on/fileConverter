5ecf3a2548393c11b7082fabde7e31c878d40ce6
sunghye-on <opei71@naver.com>
2020-07-20 16:11:21 +0900

    settimeout으로 진행바 기본틀 완성

--- client/src/components/views/Processing/Processing.js
+++ client/src/components/views/Processing/Processing.js
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Progress, Spin } from "antd";
 
 function Processing() {
-  return <div>로딩중</div>;
+  const [Percent, setPercent] = useState(0);
+  let numberDiff = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  let timeDiff = [
+    2000,
+    1000,
+    3000,
+    500,
+    100,
+    600,
+    1500,
+    1222,
+    3000,
+    3222,
+    3060,
+  ];
+  const rand = parseInt(Math.random() * 10 - 1);
+  useEffect(() => {
+    let p = Percent;
+    // let rand = parseInt(Math.random() * 12);
+    console.log(numberDiff[rand], p);
+    if (p >= 100) {
+      console.log("done");
+    } else {
+      setTimeout(async () => {
+        p = setPercent((Percent) => Percent + numberDiff[rand]);
+        return p;
+      }, timeDiff[rand]);
+    }
+  }, [Percent]);
+
+  return (
+    <div
+      style={{
+        width: "70%",
+        margin: "0 auto",
+        marginTop: "300px",
+      }}
+    >
+      <div style={{ textAlign: "center" }}>
+        <Spin size="large" tip="변환중..." />
+      </div>
+      <Progress
+        strokeColor={{
+          "0%": "#108ee9",
+          "100%": "#87d068",
+        }}
+        percent={Percent}
+        status="active"
+      />
+    </div>
+  );
 }
 
 export default Processing;
